16ebbaf414ded80561a1aa93713008f8090f8eb1
wang519735 <409083393@qq.com>
2017-09-10 21:01:57 +0800

    项目导入

---
+++ src/com/sz/wbc/servlet/FirstServlet.java
@@ -0,0 +1,19 @@
+package com.sz.wbc.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("222222222222");
+	}
+
+}
